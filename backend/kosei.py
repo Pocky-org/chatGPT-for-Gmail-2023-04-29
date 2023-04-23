c17035dcbbc23a672f817c4d120f41ae0787d9e3
hikari-8 <kanbarusuruga0127@gmail.com>
2023-04-24 01:44:30 +0900

    add connecting with front and backend API

--- backend/kosei.py
+++ backend/kosei.py
@@ -1,6 +1,6 @@
-import json
 from flask import Flask, request, Response
-from urllib import request
+from urllib.request import Request, urlopen
+import json
 from flask_cors import CORS
 
 
@@ -24,8 +24,8 @@ def post(query):
       }
     }
     params = json.dumps(param_dic).encode()
-    req = request.Request(URL, params, headers)
-    with request.urlopen(req) as res:
+    req = Request(URL, params, headers)
+    with urlopen(req) as res:
         body = res.read()
     return json.loads(body.decode())
 
@@ -41,92 +41,10 @@ def test():
 # 本番
 @app.route('/api/process-input', methods=['POST'])
 def process_input():
-    input_text = request.json['input']
+    input_text = request.get_json()['input']
     response = post(input_text)
     response_data = json.dumps(response, ensure_ascii=False)
     return Response(response_data, content_type='application/json; charset=utf-8')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
-
-
-
-
-
-# import json
-# from urllib import request
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-
-# APPID = "dj00aiZpPU5vR0Z4UXFJNHFaWSZzPWNvbnN1bWVyc2VjcmV0Jng9MzE-"  
-# URL = "https://jlp.yahooapis.jp/KouseiService/V2/kousei"
-
-# app = Flask(__name__)
-# CORS(app)
-
-# def post(query):
-#     headers = {
-#         "Content-Type": "application/json",
-#         "User-Agent": "Yahoo AppID: {}".format(APPID),
-#     }
-#     param_dic = {
-#       "id": "1234-1",
-#       "jsonrpc" : "2.0",
-#       "method": "jlp.kouseiservice.kousei",
-#       "params" : {
-#          "q": query
-#       }
-#     }
-#     params = json.dumps(param_dic).encode()
-#     req = request.Request(URL, params, headers)
-#     with request.urlopen(req) as res:
-#         body = res.read()
-#     return body.decode()
-
-# @app.route('/api/process-input', methods=['POST'])
-# def process_input():
-#     input_text = request.json['input']
-#     response = post(input_text)
-#     return jsonify({'result': response})
-
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=5000)
-
-
-
-
-
-
-
-
-# ---------------------------------------------------------------------------------------
-
-# import json
-# from urllib import request
-
-# APPID = "dj00aiZpPU5vR0Z4UXFJNHFaWSZzPWNvbnN1bWVyc2VjcmV0Jng9MzE-"  
-# URL = "https://jlp.yahooapis.jp/KouseiService/V2/kousei"
-
-
-# def post(query):
-#     headers = {
-#         "Content-Type": "application/json",
-#         "User-Agent": "Yahoo AppID: {}".format(APPID),
-#     }
-#     param_dic = {
-#       "id": "1234-1",
-#       "jsonrpc" : "2.0",
-#       "method": "jlp.kouseiservice.kousei",
-#       "params" : {
-#          "q": query
-#       }
-#     }
-#     params = json.dumps(param_dic).encode()
-#     req = request.Request(URL, params, headers)
-#     with request.urlopen(req) as res:
-#         body = res.read()
-#     return body.decode()
-
-
-# response = post("セキュリティー,食べれる")
-# print(response)

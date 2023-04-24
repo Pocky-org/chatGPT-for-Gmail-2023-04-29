62de26ecac5bdd29512158a302a974e26466a95a
yuto.matsuda <yuto.matsuda04@gmail.com>
2023-04-25 01:03:30 +0900

    update

---
+++ contents/plasmo_inline.tsx
@@ -0,0 +1,27 @@
+import type { PlasmoCSConfig, PlasmoGetInlineAnchor } from "plasmo"
+
+import someCoolImage from "data-base64:~assets/pocky.png"
+
+export const config: PlasmoCSConfig = {
+    matches: ["https://mail.google.com/*"]
+}
+
+export const getInlineAnchor: PlasmoGetInlineAnchor = () =>document.querySelector(".gU.Up")
+
+// Use this to optimize unmount lookups
+// export const getShadowHostId = () => "plasmo-inline-example-unique-id"
+
+const PlasmoInline = () => {
+    return (
+        <button>
+            <img
+            style={{
+                borderRadius: '50%',
+                width: '30px',
+            }}
+            src={someCoolImage} alt="Some pretty cool image" />
+        </button>
+    )
+}
+
+export default PlasmoInline

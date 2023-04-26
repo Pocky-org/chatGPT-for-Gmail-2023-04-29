dcfbd6ffa66d748128f8a8f47b7a37074e71e0dc
hikari-8 <kanbarusuruga0127@gmail.com>
2023-04-26 23:01:56 +0900

    add tailwind css

--- src/contents/modal_overlay.tsx
+++ src/contents/modal_overlay.tsx
@@ -1,40 +1,53 @@
+import cssText from "data-text:~/src/style.css"
 import type { PlasmoCSConfig } from "plasmo"
 import type { PlasmoGetOverlayAnchor } from "plasmo"
 import React from "react"
+
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
+}
 
 export const config: PlasmoCSConfig = {
   matches: ["https://mail.google.com/*"]
 }
 
 export const getOverlayAnchor: PlasmoGetOverlayAnchor = () =>
-  document.querySelector(".gU.Up")
+  document.querySelector("ip adB")
 
 const ModalOverlay = (props) => {
+  const { showFlag, setShowModal, response } = props
   const closeModal = () => {
-    props.setShowModal(false)
+    setShowModal(false)
   }
   return (
     <>
-      {props.showFlag && props.response ? (
-        <div
-          id="overlay"
-          style={{
-            position: "fixed",
-            top: "50%",
-            left: "50%",
-            width: 400,
-            height: 400,
-            backgroundColor: "white",
-            transform: "translate(-50%, -50%)",
-            textAlign: "center",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            zIndex: 1000
-          }}>
-          <div id="modalContent">
-            <div onClick={closeModal}>✖️</div>
-            <p className="text-3xl font-bold underline">{props.response}</p>
+      {showFlag && response ? (
+        <div>
+          <div
+            id="overlay"
+            style={{
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              width: 400,
+              height: 400,
+              backgroundColor: "white",
+              transform: "translate(-50%, -50%)",
+              textAlign: "center",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              zIndex: 1000
+            }}>
+            <div id="modalContent">
+              <div onClick={closeModal}>✖️</div>
+              <div className="bg-black text-white">
+                あああああああああああああああああああああああああああああ
+              </div>
+              <p className="text-3xl font-bold underline">{response}</p>
+            </div>
           </div>
         </div>
       ) : (
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,14 +1,18 @@
 import React from "react"
 import { useState } from "react"
 
+import "../style.css"
+
 import { sendToBackground } from "@plasmohq/messaging/"
 
 function IndexPopup() {
   const [response, setContext] = useState("")
 
   return (
-    <div>
+    <div className="p-4 m-5 w-60 gap-y-2">
+      <div className="font-bold py-2">Welcome To Pocky!!</div>
       <button
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         onClick={async () => {
           const queryOptions = { active: true, currentWindow: true }
           console.log("queryOptions: " + queryOptions)
@@ -23,7 +27,7 @@ function IndexPopup() {
         generate
       </button>
 
-      <p>res: {response}</p>
+      <p className="mt-2">res: {response}</p>
     </div>
   )
 }

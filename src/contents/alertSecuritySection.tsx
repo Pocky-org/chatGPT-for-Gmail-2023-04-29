bca4907622e5db343b8f982082fa3896e5fb9491
hikari-8 <kanbarusuruga0127@gmail.com>
2023-04-29 15:14:43 +0900

    feature add alert modal

--- src/contents/alertSecuritySection.tsx
+++ src/contents/alertSecuritySection.tsx
@@ -13,33 +13,41 @@ export const getStyle = () => {
   return style
 }
 
-const AlertSecuritySection: React.FC = () => {
+type AlertProps = {
+  isShowAlert: boolean
+}
+
+const AlertSecuritySection: React.FC<AlertProps> = (props) => {
   return (
-    <div>
-      <div
-        className="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
-        role="alert">
-        <svg
-          aria-hidden="true"
-          className="flex-shrink-0 inline w-5 h-5 mr-3"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg">
-          <path
-            fill-rule="evenodd"
-            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-            clip-rule="evenodd"></path>
-        </svg>
-        <span className="sr-only">Info</span>
+    <>
+      {props.isShowAlert && (
         <div>
-          <span className="font-medium text-sm">Warning!</span>{" "}
-          メールアドレスや電話番号が含まれている可能性があります！
-          <br />
-          返信を作成
-          (ChatGPTに送信)する前に、不必要な個人情報の削除を推奨いたします。
+          <div
+            className="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+            role="alert">
+            <svg
+              aria-hidden="true"
+              className="flex-shrink-0 inline w-5 h-5 mr-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill-rule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                clip-rule="evenodd"></path>
+            </svg>
+            <span className="sr-only">Info</span>
+            <div>
+              <span className="font-medium text-sm">Warning!</span>{" "}
+              メールアドレスや電話番号が含まれている可能性があります！
+              <br />
+              返信を作成
+              (ChatGPTに送信)する前に、不必要な個人情報の削除を推奨いたします。
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
--- src/contents/modal.tsx
+++ src/contents/modal.tsx
@@ -41,6 +41,7 @@ export default function Modal(arg: ModalProps) {
   const [chatGPTContext, setChatGPTContext] = useState("")
   const [proofreadContext, setProofreadContext] = useState<ReadPloofData[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isShowAlert, setIsShowAlert] = useState<boolean>(false)
 
   /**
    * 初回レンダリングのみ
@@ -49,6 +50,8 @@ export default function Modal(arg: ModalProps) {
     ;(async () => {
       const context = await getContext()
       setEmailContext(context)
+      const showAlertFlag = hasEmailContentPersonalInfo(context)
+      setIsShowAlert(showAlertFlag)
     })()
   }, [])
 
@@ -63,11 +66,9 @@ export default function Modal(arg: ModalProps) {
           <div className="w-full flex items-center justify-between">
             {/* タイトル */}
             <div className="flex items-end font-bold tracking-tight text-[#2FAF9C]">
-              <p className=' text-lg'>
-              Mail Butler
-              </p>
-              <p className=' ml-1 text-base'>
-              - write email with AI and get proofread
+              <p className=" text-lg">Mail Butler</p>
+              <p className=" ml-1 text-base">
+                - write email with AI and get proofread
               </p>
             </div>
             {/* 閉じるボタン */}
@@ -88,35 +89,32 @@ export default function Modal(arg: ModalProps) {
               </svg>
             </button>
           </div>
-          <div id="overlay" className=' overflow-y-auto '>
+          <div id="overlay" className=" overflow-y-auto ">
             <div id="modalContent" className="gap-y-2 grid">
               {/* メール内容 */}
               {emailContext && (
                 <div>
                   <div className="text-sm  text-[#666]">対象メール</div>
+                  <AlertSecuritySection isShowAlert={isShowAlert} />
                   <div>
-                      <textarea
-                        rows={4}
-                        className="font-medium w-full overflow-y-auto block p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                        value={emailContext}
-                        onChange={(event) => setEmailContext(event.target.value)}
-                      />
+                    <textarea
+                      rows={4}
+                      className="font-medium w-full overflow-y-auto block p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                      value={emailContext}
+                      onChange={(event) => setEmailContext(event.target.value)}
+                    />
                   </div>
                 </div>
               )}
               {/* 返信内容 */}
               <div>
                 <div className="mt-2 block text-sm text-[#666]">
-                  {emailContext? (
+                  {emailContext ? (
                     // 返信
-                    <p>
-                      返答する内容を記述してください。
-                    </p>
-                  ): (
+                    <p>返答する内容を記述してください。</p>
+                  ) : (
                     // 新規
-                    <p>
-                      送信する内容を記述してください。
-                    </p>
+                    <p>送信する内容を記述してください。</p>
                   )}
                 </div>
                 {/* 返信prompt作成部分&ChatGPTgetchボタン */}
@@ -126,10 +124,12 @@ export default function Modal(arg: ModalProps) {
                       rows={1}
                       className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2"
                       value={emailResRequest}
-                      onChange={(event) => setEmailResRequest(event.target.value)}
+                      onChange={(event) =>
+                        setEmailResRequest(event.target.value)
+                      }
                     />
                   </div>
-                  <div className='max-w-[15%]'>
+                  <div className="max-w-[15%]">
                     {isLoading ? (
                       <button
                         disabled
@@ -150,7 +150,7 @@ export default function Modal(arg: ModalProps) {
                             fill="currentColor"
                           />
                         </svg>
-                        <p className=' whitespace-nowrap font-medium text-xs text-white'>
+                        <p className=" whitespace-nowrap font-medium text-xs text-white">
                           取得中...
                         </p>
                       </button>
@@ -159,16 +159,17 @@ export default function Modal(arg: ModalProps) {
                         className="ml-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300  rounded-lg  px-4 py-2 focus:outline-none"
                         onClick={async () => {
                           setIsLoading(true)
-                          const chatGptResponse = await fetchedChatGptFromContext(
-                            emailContext,
-                            emailResRequest
-                          )
+                          const chatGptResponse =
+                            await fetchedChatGptFromContext(
+                              emailContext,
+                              emailResRequest
+                            )
                           setChatGPTContext(chatGptResponse)
                           setIsLoading(false)
                         }}>
-                          <p className=' whitespace-nowrap text-xs font-medium'>
-                            返信を生成
-                          </p>
+                        <p className=" whitespace-nowrap text-xs font-medium">
+                          返信を生成
+                        </p>
                       </button>
                     )}
                   </div>
@@ -178,24 +179,25 @@ export default function Modal(arg: ModalProps) {
               {chatGPTContext && (
                 <div className="mt-2">
                   <div>
-                    <p className='block text-sm text-[#444]'>
+                    <p className="block text-sm text-[#444]">
                       返信内容（編集可）
                     </p>
                     <textarea
                       rows={10}
                       className="font-medium w-full overflow-y-auto  block p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                       value={chatGPTContext}
-                      onChange={(event) => setChatGPTContext(event.target.value)}
+                      onChange={(event) =>
+                        setChatGPTContext(event.target.value)
+                      }
                     />
                   </div>
-                  <div
-                    className=' mt-1 flex justify-end items-end text-sm'>
+                  <AlertSecuritySection isShowAlert={isShowAlert} />
+                  <div className=" mt-1 flex justify-end items-end text-sm">
                     <button
                       className="w-[15%] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-4 py-2 focus:outline-none"
                       onClick={async () => {
-                        const proofreadResponse = await fetchedProofreadFromContext(
-                          chatGPTContext
-                        )
+                        const proofreadResponse =
+                          await fetchedProofreadFromContext(chatGPTContext)
                         setProofreadContext(proofreadResponse)
                       }}>
                       校正する
@@ -216,10 +218,8 @@ export default function Modal(arg: ModalProps) {
                 <div>
                   <ReadProofDataList data={proofreadContext} />
                 </div>
-              ):(
-                <p>
-                  校正すべき箇所はありませんでした。
-                </p>
+              ) : (
+                <p>校正すべき箇所はありませんでした。</p>
               )}
             </div>
           </div>
@@ -274,6 +274,21 @@ const removeMessagePart = (input) => {
   const messageToRemove =
     /\.{3}\[メッセージの一部が表示されています\]\s{1,}メッセージ全体を表示/
   return email.replace(messageToRemove, "")
+}
+/**
+ * メールのセキュリティチェック
+ */
+function hasEmailContentPersonalInfo(input: string): boolean {
+  // emailContextの中にメールや電話番号が入っているかどうかをチェックする
+  // メールアドレスの正規表現
+  const emailRegex = /\S+@\S+\.\S+/
+
+  // 電話番号の正規表現 (この例では、国際電話番号も含まれます)
+  const phoneRegex =
+    /(\+\d{1,3}[-\.\s]?)?\(?\d{1,4}\)?[-\.\s]?\d{1,4}[-\.\s]?\d{1,9}/
+
+  // 文字列にメールアドレスまたは電話番号が含まれているかどうかをチェック
+  return emailRegex.test(input) || phoneRegex.test(input)
 }
 
 /**
